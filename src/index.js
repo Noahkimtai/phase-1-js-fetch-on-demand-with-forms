f9630716e0618738566774015d2242fd05578cce
Noahkimtai <noahkimtai24@gmail.com>
2022-08-27 12:45:34 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
-  
-}
+    let form = document.querySelector('form');
+    form.addEventListener('submit', (e)=>{
+        e.preventDefault()
+        let input = e.target.children[1].value
+        
+        fetch(`http://localhost:3000/movies/${input}`).then(res =>res.json())
+        .then(data =>{
+            let title = document.querySelector('section#movieDetails h4')
+            let summary = document.querySelector('section#movieDetails p')
+            // change the inner text of the above elements
+            title.innerText = data.title
+            summary.innerText =data.summary
+        })
+        //input.reset()
+    })
+}  
 
 document.addEventListener('DOMContentLoaded', init);
